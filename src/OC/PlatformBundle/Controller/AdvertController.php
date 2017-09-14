c442006a62aacba5a2a868c51cc4cd69d4549eb8
momise92 <boss973@hotmail.fr>
2017-09-14 12:43:13 +0200

    modification route et controller pour les nouvelles pages

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,21 +4,39 @@
 
 namespace OC\PlatformBundle\Controller;
 
-// N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+  // La route fait appel à OCPlatformBundle:Advert:view,
+  // on doit donc définir la méthode viewAction.
+  // On donne à cette méthode l'argument $id, pour
+  // correspondre au paramètre {id} de la route
+    public function viewAction($id)
+    {
+        // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+        // Ici, on récupèrera depuis la base de données
+        // l'annonce correspondant à l'id $id.
+        // Puis on passera l'annonce à la vue pour
+        // qu'elle puisse l'afficher
+
+        return new Response("Affichage de l'annonce d'id : ".$id);
+    }
+    
+    // ... et la méthode indexAction que nous avons déjà créée
     public function indexAction()
     {
         return $this->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Carter'));
-
     }
     
-    public function byebyeAction()
+    // On récupère tous les paramètres en arguments de la méthode
+    public function viewSlugAction($slug, $year, $_format)
     {
-        return $this->render('OCPlatformBundle:Advert:byebye.html.twig', array('nom' => 'Carter'));
-
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        );
     }
 }
